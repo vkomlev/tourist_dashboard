30cc39aed62a178ebed6bcdc264fff4ac0ab9f5a
Victor Komlev <victor.v.komlev@gmail.com>
2024-06-08 19:14:31 +0500

    Добавил popup с ссылкой на дашборд региона

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # Кэш для хранения GeoDataFrame
 cached_gdf = None
-
+#
 def load_geojson():
     global cached_gdf
     if cached_gdf is None:
@@ -38,60 +38,35 @@ def index():
     gdf = load_geojson()
     m = folium.Map(location=[61.5240, 105.3188], zoom_start=3)
 
-    # Стиль для GeoJson
-    def style_function(feature):
-        region_name = feature['properties']['name:ru']
+    # Добавление Popup с информацией о регионе
+    for _, row in gdf.iterrows():
+        region_name = row['name:ru']
         region_name_translit = transliterate(region_name)
-        return {
-            'fillColor': "#{:06x}".format(random.randint(0, 0xFFFFFF)),
-            'color': "#{:06x}".format(random.randint(0, 0xFFFFFF)),
-            'weight': 2,
-            'fillOpacity': 0.5,
-            'className': f"region-{region_name_translit}"  # Используем транслитерированное имя для класса
-        }
-
-    # Создание GeoJson объекта
-    geojson = folium.GeoJson(
-        gdf,
-        style_function=style_function,
-        highlight_function=lambda feature: {
-            'weight': 3,
-            'color': 'black',
-            'fillOpacity': 0.7
-        },
-        tooltip=folium.GeoJsonTooltip(
-            fields=['name:ru'],
-            aliases=['Регион: '],
-            labels=True,
-            sticky=False
-        ),       
-    ).add_to(m)
-
-    # JavaScript для обработки клика по региону
-    m.get_root().html.add_child(folium.Element("""
-        <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            var regionElements = document.querySelectorAll('[class^="region-"]');
-            for (var i = 0; i < regionElements.length; i++) {
-                regionElements[i].addEventListener('click', function() {
-                    var regionClass = this.getAttribute('class');
-                    var regionName = regionClass.split('region-')[1];
-                    if (regionName) {
-                        window.location.href = `/region/${regionName}`;
-                    }
-                });
-            }
-        });
-        </script>
-    """))
+        popup_content = f"<b>{region_name}</b><br><a href='/region/{region_name_translit}'>Подробнее</a>"
+        popup = folium.Popup(popup_content, max_width=300)
+        color = "#{:06x}".format(random.randint(0, 0xFFFFFF))  # Генерация случайного цвета
+        folium.GeoJson(
+            row['geometry'],
+            style_function=lambda feature, color=color: {
+                'fillColor': "#{:06x}".format(random.randint(0, 0xFFFFFF)),
+                'color': "#{:06x}".format(random.randint(0, 0xFFFFFF)),
+                'weight': 2,
+                'fillOpacity': 0.5,
+            },
+            highlight_function=lambda feature: {
+                'weight': 3,
+                'color': 'black',
+                'fillOpacity': 0.7
+            },
+            tooltip=row['name:ru'],
+            popup=popup
+        ).add_to(m)
 
     map_html = m._repr_html_()
     return render_template('index.html', map_html=map_html)
 
 @main.route('/region/<name>')
-def region(name):
-    name = transliterate(name)  # Если нужно обратное преобразование
-    #return f"<h1>{region_name}</h1><a href='/'>Вернуться на главную</a>"
+def region(name):    
     return render_template('region.html', region_name=name)
 
 
